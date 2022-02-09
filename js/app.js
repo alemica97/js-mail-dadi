2acf079c74bd99756c22cbbd83c2ddcee44f5a82
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-09 16:42:48 +0100

    inizio dell'esercizio, ho scritto i commenti per capire come svolgere l'esercizio

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// -------------------------MAIL--------------------------------
+
+//creo una lista di email
+
+//chiedo all'utente la sua email
+
+//SE l'email è corretta stampo un messaggio positivo
+
+//ALTRIMENTI stampo un messaggio d'errore
+
+
+
+
+
+
+
+
+
+
+// -----------------------------DADI------------------------------
+
+//genero due numeri random, uno per l'utente e uno per il computer, che vadano da 1 a 6
+
+//SE il numero dell'utente è > del numero del computer vince l'utente
+
+//SE il numero dell'utente è < del numero del computer vince il computer
+
+//ALTRIMENTI è un pareggio
